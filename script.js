3119a24b9fdc9290d86f0e00275b143960c589ac
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-23 13:15:59 -0500

    download button allows a jpeg image of the canvas to be downloaded in its current state. All UI elements wired and functioning.

--- script.js
+++ script.js
@@ -194,13 +194,14 @@ clearStorageBtn.addEventListener('click', () => {
     setTimeout(switchToBrush, 1500);
 });
 
-// // Download Image
-// downloadBtn.addEventListener('click', () => {
-
-//   // Active Tool
-//   activeToolEl.textContent = 'Image File Saved';
-//   setTimeout(switchToBrush, 1500);
-// });
+// Download Image
+downloadBtn.addEventListener('click', () => {
+    downloadBtn.href = canvas.toDataURL('image/jpeg', 1);
+    downloadBtn.download = 'paint-example.jpeg';
+    // Active Tool
+    activeToolEl.textContent = 'Image File Saved';
+    setTimeout(switchToBrush, 1500);
+});
 
 // // Event Listener
 brushIcon.addEventListener('click', switchToBrush);
